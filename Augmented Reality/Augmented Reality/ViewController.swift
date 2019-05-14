d83074b2590a4b821bdfe1a4739e966876c26114
Kevin Ren <kevin@Kevins-MacBook-Pro.local>
2019-05-13 20:41:36 -0400

    AR setup code

--- Augmented Reality/Augmented Reality/ViewController.swift
+++ Augmented Reality/Augmented Reality/ViewController.swift
@@ -15,6 +15,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    func setupScene() {
+        let scene = SCNScene()
+        sceneView.scene = scene
+    }
+    
+    func setupConfiguration() {
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    func addDrone() {
+        drone.loadModel()
+        sceneView.scene.rootNode.addChildNode(drone)
+    }
+    view raw
+    
+    let kMovingLengthPerLoop: CGFloat = 0.05
+    let kRotationRadianPerLoop: CGFloat = 0.2
+    
+    @IBAction func upLongPressed(_ sender: UILongPressGestureRecognizer) {
+        let action = SCNAction.moveBy(x: 0, y: kMovingLengthPerLoop, z: 0, duration: kAnimationDurationMoving)
+        execute(action: action, sender: sender)
+    }
 }
 
